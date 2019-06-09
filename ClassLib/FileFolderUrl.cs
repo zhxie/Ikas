3fd2550ea7e2e04d20335940ecca370a353bff1c
Xie Zhihao <xzh1206@gmail.com>
2019-06-10 00:41:51 +0800

    Add FileFolderUrl

---
+++ ClassLib/FileFolderUrl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class FileFolderUrl
+    {
+        public const string SplatNet = @"https://app.splatoon2.nintendo.net";
+        public const string Schedule = @"/api/schedules";
+        public const string Battle = @"/api/results/{}";
+    }
+}
