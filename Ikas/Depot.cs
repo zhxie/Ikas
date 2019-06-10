2fe53b491a5263eb8115a3f6c97ee66b423c5c05
Xie Zhihao <xzh1206@gmail.com>
2019-06-10 22:29:30 +0800

    Show stage image on MainWindow preliminarily

---
+++ Ikas/Depot.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+
+using ClassLib;
+
+namespace Ikas
+{
+    public delegate void ScheduleUpdatedEventHandler();
+    public delegate void CurrentModeChangedEventHandler();
+    public static class Depot
+    {
+        public static event ScheduleUpdatedEventHandler ScheduleUpdatedEvent;
+        private static Mutex ScheduleMutex = new Mutex();
+        public static Schedule Schedule { get; set; } = new Schedule();
+
+        public static event CurrentModeChangedEventHandler CurrentModeChangedEvent;
+        private static Mode.Key currentMode = Mode.Key.regular_battle;
+        public static Mode.Key CurrentMode
+        {
+            get
+            {
+                return currentMode;
+            }
+            set
+            {
+                if (CurrentMode != value)
+                {
+                    currentMode = value;
+                    // Raise event
+                    CurrentModeChangedEvent?.Invoke();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get current and next Schedule in regular, ranked and league mode.
+        /// </summary>
+        public static async void GetSchedule()
+        {
+            // Send HTTP GET
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.UseCookies = false;
+            HttpClient client = new HttpClient(handler);
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, FileFolderUrl.SplatNet + FileFolderUrl.Schedule);
+            request.Headers.Add("Cookie", "iksm_session=" + "e1a7ae2de618b711fee2e7b16024dbb5766fd028");
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                string resultString = await response.Content.ReadAsStringAsync();
+                // Parse JSON
+                JObject jObject = JObject.Parse(resultString);
+                try
+                {
+                    // Regular
+                    List<ScheduledStage> regularStages = parseScheduledStages(jObject["regular"][0]);
+                    List<ScheduledStage> nextRegularStages = parseScheduledStages(jObject["regular"][1]);
+                    // Ranked
+                    List<ScheduledStage> rankedStages = parseScheduledStages(jObject["gachi"][0]);
+                    List<ScheduledStage> nextRankedStages = parseScheduledStages(jObject["gachi"][1]);
+                    // League
+                    List<ScheduledStage> leagueStages = parseScheduledStages(jObject["league"][0]);
+                    List<ScheduledStage> nextLeagueStages = parseScheduledStages(jObject["league"][1]);
+                    // Create Schedule
+                    DateTime endTime = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(int.Parse(jObject["regular"][0]["end_time"].ToString()));
+                    List<ScheduledStage> stages = new List<ScheduledStage>(), nextStages = new List<ScheduledStage>();
+                    stages.AddRange(regularStages);
+                    stages.AddRange(rankedStages);
+                    stages.AddRange(leagueStages);
+                    nextStages.AddRange(nextRegularStages);
+                    nextStages.AddRange(nextRankedStages);
+                    nextStages.AddRange(nextLeagueStages);
+                    // Update Schedule
+                    UpdateSchedule(new Schedule(endTime, stages, nextStages));
+                }
+                catch
+                {
+                    // Update Schedule
+                    UpdateSchedule(new Schedule());
+                }
+            }
+            else
+            {
+                // Update Schedule
+                UpdateSchedule(new Schedule());
+            }
+        }
+        /// <summary>
+        /// Update Schedule.
+        /// </summary>
+        public static bool UpdateSchedule(Schedule schedule)
+        {
+            if (Schedule != schedule)
+            {
+                ScheduleMutex.WaitOne();
+                Schedule = schedule;
+                ScheduleMutex.ReleaseMutex();
+                // Raise event
+                ScheduleUpdatedEvent?.Invoke();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Parse ScheduledStages from JToken.
+        /// </summary>
+        /// <param name="node">JToken of a schedule of certain mode</param>
+        private static List<ScheduledStage> parseScheduledStages(JToken node)
+        {
+            List<ScheduledStage> stages = new List<ScheduledStage>();
+            try
+            {
+                Mode.Key mode = Mode.ParseKey(node["game_mode"]["key"].ToString());
+                Rule.Key rule = Rule.ParseKey(node["rule"]["key"].ToString());
+                ScheduledStage stage1, stage2;
+                if (int.TryParse(node["stage_a"]["id"].ToString(), out int stage1Id))
+                {
+                    string image = node["stage_a"]["image"].ToString();
+                    stage1 = new ScheduledStage(mode, rule, (Stage.Key)stage1Id, image);
+                }
+                else
+                {
+                    stage1 = new ScheduledStage(mode, rule);
+                }
+                if (int.TryParse(node["stage_b"]["id"].ToString(), out int stage2Id))
+                {
+                    string image = node["stage_b"]["image"].ToString();
+                    stage2 = new ScheduledStage(mode, rule, (Stage.Key)stage2Id, image);
+                }
+                else
+                {
+                    stage2 = new ScheduledStage(mode, rule);
+                }
+                stages.Add(stage1);
+                stages.Add(stage2);
+            }
+            catch
+            {
+                return new List<ScheduledStage>();
+            }
+            return stages;
+        }
+    }
+}
--- Ikas/MainWindow.xaml.cs
+++ Ikas/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+using ClassLib;
+
 namespace Ikas
 {
     /// <summary>
@@ -23,6 +28,54 @@ namespace Ikas
         public MainWindow()
         {
             InitializeComponent();
+            // Set properties for controls
+            RenderOptions.SetBitmapScalingMode(bdStage1, BitmapScalingMode.HighQuality);
+            RenderOptions.SetBitmapScalingMode(bdStage2, BitmapScalingMode.HighQuality);
+            // Update Schedule
+            Depot.GetSchedule();
+            Depot.ScheduleUpdatedEvent += new ScheduleUpdatedEventHandler(ScheduleUpdated);
+        }
+
+        private void ScheduleUpdated()
+        {
+            // TODO: brush animation
+            // TODO: download image if not exist
+            Schedule schedule = Depot.Schedule;
+            List<ScheduledStage> scheduledStages = schedule.GetStages(Depot.CurrentMode);
+            if (scheduledStages.Count > 0 || Depot.CurrentMode == Mode.Key.regular_battle)
+            {
+                if (scheduledStages.Count > 0)
+                {
+                    Stage stage = scheduledStages[0];
+                    string image = System.Environment.CurrentDirectory + stage.Image;
+                    if (File.Exists(image))
+                    {
+                        ImageBrush brush = new ImageBrush(new BitmapImage(new Uri(image)));
+                        brush.Stretch = Stretch.UniformToFill;
+                        bdStage1.Background = brush;
+                    }
+                    else
+                    {
+
+                    }
+                }
+                if (scheduledStages.Count > 1)
+                {
+                    Stage stage = scheduledStages[1];
+                    string image = System.Environment.CurrentDirectory + stage.Image;
+                    if (File.Exists(image))
+                    {
+                        ImageBrush brush = new ImageBrush(new BitmapImage(new Uri(image)));
+                        brush.Stretch = Stretch.UniformToFill;
+                        bdStage2.Background = brush;
+                    }
+                }
+            }
+            else
+            {
+                // Current mode do not has a schedule, switch to regular battle
+                Depot.CurrentMode = Mode.Key.regular_battle;
+            }
         }
     }
 }
