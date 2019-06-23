c3a14f60bcf542dd63704102e77a78932f387567
Xie Zhihao <xzh1206@gmail.com>
2019-06-23 21:34:29 +0800

    Add MessageWindow

---
+++ Ikas/MessageWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Windows.Media.Animation;
+
+namespace Ikas
+{
+    /// <summary>
+    /// MessageWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MessageWindow : Window
+    {
+        public Window KeepAliveWindow { get; set; }
+
+        public MessageWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Control Event
+
+        private void Window_LocationChanged(object sender, EventArgs e)
+        {
+            /*
+            if (Top < 0)
+            {
+                Top = 0;
+            }
+            if (Top + Height > WpfScreen.GetScreenFrom(this).DeviceBounds.Height)
+            {
+                Top = WpfScreen.GetScreenFrom(this).DeviceBounds.Height - Height;
+            }
+            */
+        }
+
+        private void Window_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (KeepAliveWindow != null)
+            {
+                ((Storyboard)FindResource("window_fade_in")).Begin(KeepAliveWindow);
+            }
+            ((Storyboard)FindResource("window_fade_in")).Begin(this);
+        }
+
+        private void Window_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((Storyboard)FindResource("window_fade_out")).Begin(this);
+        }
+
+        #endregion
+
+        public void SetContent(string title, string content)
+        {
+            tbTitle.Text = title;
+            atContent.Text = content;
+        }
+    }
+}
