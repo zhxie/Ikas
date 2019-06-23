6f6bfc09dc844d8b3f4b3fe68f879c4fc8beaef3
Xie Zhihao <xzh1206@gmail.com>
2019-06-23 21:34:51 +0800

    Add help messages in settings

--- Ikas/SettingsWindow.xaml.cs
+++ Ikas/SettingsWindow.xaml.cs
@@ -24,6 +24,8 @@ namespace Ikas
     /// </summary>
     public partial class SettingsWindow : Window
     {
+        private MessageWindow messageWindow;
+
         public string SelectionForeground
         {
             get
@@ -79,6 +81,10 @@ namespace Ikas
             InitializeComponent();
             // Add handler for global member
             Depot.LanguageChanged += new LanguageChangedEventHandler(LanguageChanged);
+            // Prepare windows
+            messageWindow = new MessageWindow();
+            messageWindow.Opacity = 0;
+            messageWindow.Visibility = Visibility.Hidden;
         }
 
         #region Control Event
@@ -177,12 +183,16 @@ namespace Ikas
 
         private void LbWhatIsSessionToken_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            ShowMessage(Translate("Session Token is..", true),
+                string.Format(Translate("{0}\n{1}", true),
+                Translate("A Session Token is a small piece of data used for automatic cookie generation.", true),
+                Translate("Automatic cookie generation sends minimal data including the Session Token to Nintendo servers, and get cookie back for accessing SplatNet.", true)),
+                e.GetPosition(this));
         }
 
         private void LbWhatIsSessionToken_MouseLeave(object sender, MouseEventArgs e)
         {
-
+            ((Storyboard)FindResource("window_fade_out")).Begin(messageWindow);
         }
 
         private void LbGetSessionToken_MouseEnter(object sender, MouseEventArgs e)
@@ -202,12 +212,17 @@ namespace Ikas
 
         private void LbWhatIsCookie_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            ShowMessage(Translate("Cookie is..", true),
+                string.Format(Translate("{0}\n{1}\n{2}", true),
+                Translate("A Cookie is a small piece of data sent from a website and stored on the user's computer.", true),
+                Translate("Ikas uses cookies to access SplatNet and get schedule and battle data.", true),
+                Translate("If you don't know how to get a cookie, you may fill out the Session Token, and the system will get one automatically.", true)),
+                e.GetPosition(this));
         }
 
         private void LbWhatIsCookie_MouseLeave(object sender, MouseEventArgs e)
         {
-
+            ((Storyboard)FindResource("window_fade_out")).Begin(messageWindow);
         }
 
         private void LbUpdateCookie_MouseEnter(object sender, MouseEventArgs e)
@@ -283,12 +298,17 @@ namespace Ikas
 
         private void LbWhatIsClearCache_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            ShowMessage(Translate("Clear Cache is..", true),
+                string.Format(Translate("{0}\n{1}\n{2}", true),
+                Translate("When Ikas gets schedule and battle data, it will cache images including user icons, stages, weapons and gears.", true),
+                Translate("Sometimes, due to network or other reasons, Ikas may not get images properly, which may cause wrong cached images.", true),
+                Translate("You can help Ikas get back to normal operation by clearing the cached data.", true)),
+                e.GetPosition(this));
         }
 
         private void LbWhatIsClearCache_MouseLeave(object sender, MouseEventArgs e)
         {
-
+            ((Storyboard)FindResource("window_fade_out")).Begin(messageWindow);
         }
 
         #endregion
@@ -301,6 +321,14 @@ namespace Ikas
                 Resources.MergedDictionaries.Clear();
             }
             Resources.MergedDictionaries.Add(lang);
+        }
+
+        private void ShowMessage(string title, string content, Point point)
+        {
+            messageWindow.SetContent(title, content);
+            messageWindow.Top = point.Y + Top - messageWindow.Height / 2;
+            messageWindow.Left = point.X + Left + 10;
+            ((Storyboard)FindResource("window_fade_in")).Begin(messageWindow);
         }
 
         private string Translate(string s, bool isLocal = false)
