5b0110dfac6ffb0551097b9f64b69c56e38e80ad
Xie Zhihao <xzh1206@gmail.com>
2019-06-25 23:38:09 +0800

    Update 0.1.0

--- Ikas/Properties/AssemblyInfo.cs
+++ Ikas/Properties/AssemblyInfo.cs
@@ -53,4 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.1 RC")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
