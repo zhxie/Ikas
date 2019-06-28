cbdb6ed9568357490287110dbe8d227054f0a73e
Xie Zhihao <xzh1206@gmail.com>
2019-06-29 00:17:32 +0800

    Update color scheme

--- ClassLib/Design.cs
+++ ClassLib/Design.cs
@@ -11,5 +11,7 @@ namespace ClassLib
         public const string NeonGreen = "19D719";
         public const string NeonOrange = "F54910";
         public const string NeonRed = "F02D7D";
+
+        public const string NeonYellow = "CDF523";
     }
 }
