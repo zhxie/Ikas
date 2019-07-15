b5d665df7990a9b0939f053051ecac136c50c866
Xie Zhihao <xzh1206@gmail.com>
2019-07-16 00:22:47 +0800

    Add Ikas.Notification

---
+++ Ikas.Notification/NotificationActivator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+
+using System.Runtime.InteropServices;
+
+using Ikas.Notification.ShellHelpers;
+
+namespace Ikas.Notification
+{
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComSourceInterfaces(typeof(INotificationActivationCallback))]
+    [Guid("16F7ED7A-C091-418C-8FC4-B447FF9CEFB0"), ComVisible(true)]
+    public class NotificationActivator : INotificationActivationCallback
+    {
+        public void Activate(string appUserModelId, string invokedArgs, NOTIFICATION_USER_INPUT_DATA[] data, uint dataCount)
+        {
+            NotificationHelper.InvokeToastActivated();
+        }
+
+        public static void Initialize()
+        {
+            regService = new RegistrationServices();
+
+            cookie = regService.RegisterTypeForComClients(
+                typeof(NotificationActivator),
+                RegistrationClassContext.LocalServer,
+                RegistrationConnectionType.MultipleUse);
+        }
+        public static void Uninitialize()
+        {
+            if (cookie != -1 && regService != null)
+            {
+                regService.UnregisterTypeForComClients(cookie);
+            }
+        }
+
+        private static int cookie = -1;
+        private static RegistrationServices regService = null;
+    }
+}
---
+++ Ikas.Notification/NotificationHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ikas.Notification
+{
+    public delegate void ToastActivatedEventHandler();
+    public static class NotificationHelper
+    {
+        public static event ToastActivatedEventHandler ToastActivated = null;
+
+        public static void InvokeToastActivated()
+        {
+            if (ToastActivated != null)
+            {
+                ToastActivated.Invoke();
+            }
+        }
+    }
+}
