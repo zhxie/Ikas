74b323c6340e7943359e58e57e0292dcd1d96545
Xie Zhihao <xzh1206@gmail.com>
2019-07-18 00:44:14 +0800

    Seperate toast notifications into Ikas and Ikas (Debug)

--- Ikas.Notification/IkasNotificationActivator.cs
+++ Ikas.Notification/IkasNotificationActivator.cs
@@ -16,10 +16,18 @@ namespace Ikas.Notification
     // The GUID CLSID must be unique to your app. Create a new GUID if copying this code.
     [ClassInterface(ClassInterfaceType.None)]
     [ComSourceInterfaces(typeof(INotificationActivationCallback))]
+#if DEBUG
     [Guid("a6f70341-f83c-4ba8-97de-1c6947715679"), ComVisible(true)]
+#else
+    [Guid("f9fa6e5b-ea57-4d16-9cf1-adeb57e110a1"), ComVisible(true)]
+#endif
     public class IkasNotificationActivator : NotificationActivator
     {
+#if DEBUG
+        public const string AppId = "Ikas (Debug)";
+#else
         public const string AppId = "Ikas";
+#endif
 
         public override void OnActivated(string arguments, NotificationUserInput userInput, string appUserModelId)
         {
