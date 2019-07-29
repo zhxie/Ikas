25ccc0235e4c32bc11787343ebaad57a01f97a32
Xie Zhihao <xzh1206@gmail.com>
2019-07-29 22:14:41 +0800

    Release 0.2.2

--- Ikas.Notification/Properties/AssemblyInfo.cs
+++ Ikas.Notification/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.1.0")]
+[assembly: AssemblyVersion("0.2.2.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.2.1")]
+[assembly: AssemblyInformationalVersion("0.2.2")]
