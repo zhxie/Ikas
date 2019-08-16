d4eeb9433df8ee0e225e85ccb9033b2fe049b418
Xie Zhihao <xzh1206@gmail.com>
2019-08-16 23:50:18 +0800

    Fix translation in job player window

--- Ikas/JobPlayerWindow.xaml.cs
+++ Ikas/JobPlayerWindow.xaml.cs
@@ -39,6 +39,20 @@ namespace Ikas
 
         public JobPlayerWindow()
         {
+            // Load language
+            if (Depot.Language != null && Depot.Language != "")
+            {
+                try
+                {
+                    ResourceDictionary lang = (ResourceDictionary)Application.LoadComponent(new Uri(@"assets/lang/" + Depot.Language + ".xaml", UriKind.Relative));
+                    if (Resources.MergedDictionaries.Count > 0)
+                    {
+                        Resources.MergedDictionaries.Clear();
+                    }
+                    Resources.MergedDictionaries.Add(lang);
+                }
+                catch { }
+            }
             // Initialize component
             InitializeComponent();
             // Set properties for controls
