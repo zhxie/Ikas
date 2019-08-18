faff979619d4b7e5d25af3481ab37b4da34a95b7
Xie Zhihao <xzh1206@gmail.com>
2019-08-18 21:32:23 +0800

    Add salmoniod class

---
+++ Ikas.Class/Salmoniod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ikas.Class
+{
+    public class Salmoniod
+    {
+        public enum Key
+        {
+            goldie = 3,
+            steelhead = 6,
+            flyfish = 9,
+            scrapper = 12,
+            steelEel = 13,
+            stinger = 14,
+            maws = 15,
+            griller = 16,
+            drizzler = 21
+        }
+
+        public Key Id { get; }
+        public string Name { get; }
+
+        public Salmoniod(Key id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
